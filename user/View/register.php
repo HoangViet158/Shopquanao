f36ebb9d1981657ee87edcdf3fb6a52c63f7177a
tieep <tn5271291@gmail.com>
2025-05-13 06:01:45 +0700

    merge thêm commit login/register của việt

--- user/View/register.php
+++ user/View/register.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Cửa hàng quần áo</title>
+    <title>Cửa hàng quần áoo</title>
 </head>
+
 <body>
     <div class="container">
         <div class="image-section"></div>
@@ -33,4 +35,5 @@
             </div>
         </div>
 </body>
+
 </html>
