1551cdfacf885c39b85a61d4176f050e67dd38ec
tieep <tn5271291@gmail.com>
2025-05-14 05:24:06 +0700

    fix lại đường dẫn thành tương đối

--- user/View/Products/index.php
+++ user/View/Products/index.php
@@ -7,11 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lọc sản phẩm</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/Web2/Shopquanao/public/user/css/products.css" rel="stylesheet">
+    <link href="../../../public/user/css/products.css" rel="stylesheet">
 
 </head>
 <?php include __DIR__ . '/../header.php'; ?>
 <!-- code đc render từ file js trong thư mục public/user/js/products.js -->
+
 <body>
     <div class="container mt-4">
         <div class="row pt-4">
@@ -23,12 +24,12 @@
             <!-- Phần danh sách sản phẩm -->
             <div class="col-md-9">
                 <div id="product-list">
-                
+
                 </div>
 
                 <!-- Phân trang -->
                 <div id="pagination">
-                    
+
                 </div>
             </div>
         </div>
@@ -36,7 +37,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/Web2/Shopquanao/public/user/js/products.js"></script>
+    <script src="../../../public/user/js/products.js"></script>
 </body>
 <?php include __DIR__ . '/../footer.php'; ?>
 
